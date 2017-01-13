3c3a829006c80b4bbb72dd735fd233720d9c85fd
Derrick Wong <dcw008@ucsd.edu>
2017-01-13 13:40:19 -0800

    added scroll view to detailed listing

--- Flicks/Flicks/DetailViewController.swift
+++ Flicks/Flicks/DetailViewController.swift
@@ -13,6 +13,8 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
     
     var movie: NSDictionary!
     
@@ -20,20 +22,27 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //set the width and height of the scrollView 
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width , height: infoView.frame.origin.y + infoView.frame.size.height) //how far the screen spans + the height of the infoView
+        
+        //set title and overview
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         
+        //set the base url
+        let posterBaseUrl = "http://image.tmdb.org/t/p/w500"
+        
         // get the poster image
         if let posterPath = movie["poster_path"] as? String {
-            let posterBaseUrl = "http://image.tmdb.org/t/p/w500"
             let posterUrl = NSURL(string: posterBaseUrl + posterPath)
             posterView.setImageWith(posterUrl as! URL)
         }
         
+        //set elements of ui
         titleLabel.text = title
         overviewLabel.text = overview
+        overviewLabel.sizeToFit()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
