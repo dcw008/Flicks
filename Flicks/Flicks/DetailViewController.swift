234442743fc334efc271563d5301073e8506550f
Derrick Wong <dcw008@ucsd.edu>
2017-08-19 23:25:08 -0700

    gave up on gradient, fixed the rating, trying to do the api request

--- Flicks/Flicks/DetailViewController.swift
+++ Flicks/Flicks/DetailViewController.swift
@@ -18,7 +18,9 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var overviewLabel: UILabel!
 
-    var movie: NSDictionary!
+    var movie: NSDictionary! //movie passed through segue
+    var movieDetails: NSDictionary? //movie details from the api request
+    
     var count = 0
     
     let gradientLayer: CAGradientLayer = {
@@ -42,16 +44,26 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //set title and overview
-        let title = movie["title"] as! String
-        let overview = movie["overview"] as! String
+//        //set title and overview
+//        let title = movie["title"] as! String
+//        let overview = movie["overview"] as! String
+        
+        //get the movie id
+        let movieId = movie["id"] as! Int
+        
+        request(id: movieId)
+        
+        
+        let title = movieDetails?["title"] as! String
+        let overview = movieDetails?["overview"] as! String
+        
         
         //set the base url
         let posterBaseUrl = "http://image.tmdb.org/t/p/w500"
         
         
         // get the poster image
-        if let backdropPath = movie["backdrop_path"] as? String {
+        if let backdropPath = movieDetails?["backdrop_path"] as? String {
             let backdropUrl = NSURL(string: posterBaseUrl + backdropPath)
             backdropImage.setImageWith(backdropUrl as! URL)
             backdropImage.layer.addSublayer(gradientLayer)
@@ -80,4 +92,32 @@ class DetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    //makes a network request to detailed information about the movie
+    func request(id: Int){
+        
+        //Network api request
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/\(id)?api_key=\(apiKey)")
+        print("https://api.themoviedb.org/3/movie/\(id)?api_key=\(apiKey)&language=en-US")
+        let request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10);
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task : URLSessionDataTask = session.dataTask(with: request, completionHandler: { (dataOrNil, response, error) in
+            // ... Use the new data to update the data source ...
+            if let data = dataOrNil{
+                if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as? NSDictionary {
+                    //print(responseDictionary)
+                    
+                    self.movieDetails = responseDictionary
+                    
+                }
+            }
+        })
+        task.resume()
+        
+        
+        
+    } 
 }
